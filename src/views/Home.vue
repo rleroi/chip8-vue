64c42a1fdf55a3b87530e88d7222ecadd1600269
Ramses Leroi <r.leroi@live.nl>
2021-11-06 11:19:26 +0100

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,160 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <div style="margin-bottom: 1rem; display: flex;">
+            <div>
+                <div class="canvas-wrapper" @click="showGrid = !showGrid">
+                    <canvas width="64" height="32" ref="canvas"></canvas>
+                    <div class="grid" v-if="showGrid">
+                        <template v-for="y in 32">
+                            <div v-for="x in 64" @mouseenter="gridX = x-1; gridY = y-1"></div>
+                        </template>
+                    </div>
+                </div>
+                <div style="text-align: center; width: 512px;">({{gridX}}, {{gridY}})</div>
+            </div>
+            <div style="margin-left: 1rem;">
+                Key layout:
+                <pre>1 2 3 4</pre>
+                <pre>Q W E R</pre>
+                <pre>A S D F</pre>
+                <pre>Z X C V</pre>
+
+                <div>
+                    ROMs:
+                    <div v-for="current in roms">{{current.name}} <button @click="rom=current.rom; loadRom()">Load</button></div>
+                    <input type="file" @change="importRom" />
+                </div>
+            </div>
+        </div>
+
+        <div style="margin-bottom: 1rem;">
+            <button @click="loadRom">Restart</button>
+            <button @click="pause">Pause</button>
+            <button @click="start">Continue</button>
+            <button @click="jump">Skip</button>
+            <input type="range" :min="0" :max="1000" v-model="emulator.deltaTime" />
+            <span>delay: {{emulator.deltaTime}}</span>
+        </div>
+
+        <h2 @click="showDebug = !showDebug">Debug</h2>
+        <div v-if="showDebug">
+            <h3 @click="showCurrentTick = !showCurrentTick">CurrentTick</h3>
+            <pre v-if="showCurrentTick && emulator.currentTick.instruction && emulator.currentTick.instruction.id">
+                {{emulator.currentTick.instruction.id}},
+                {{emulator.currentTick.operands.join(', ')}}
+            </pre>
+
+            <h3 @click="showKeysDown = !showKeysDown">KeysDown</h3>
+            <pre v-if="showKeysDown">{{input.keysDown}}</pre>
+
+            <h3 @click="showRom = !showRom">ROM</h3>
+            <textarea v-if="showRom" style="width: 374px; height: 158px;" v-model="rom"></textarea>
+
+            <h3 @click="showCpu = !showCpu">CPU</h3>
+            <pre v-if="showCpu">{{cpu}}</pre>
+
+            <h3 @click="showMemory = !showMemory">Memory</h3>
+            <textarea v-if="showMemory">{{memory}}</textarea>
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+    // @ is an alias to /src
+    import {emulator, video, cpu, memory, audio, input} from '@/chip8/Chip8.js'
+    import {hexToBin, binToHex} from '@/chip8/Helpers.js'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'Home',
+        data() {
+            return {
+                roms: [
+                    {
+                        name: 'Breakout',
+                        rom: '6e0565006b066a00a30cdab17a043a4012087b023b1212066c206d1fa310dcd122f660006100a312d0117008a30ed0116040f015f00730001234c60f671e680169ffa30ed671a310dcd16004e0a17cfe6006e0a17c02603f8c02dcd1a30ed67186848794603f8602611f8712471f12ac46006801463f68ff47006901d6713f0112aa471f12aa600580753f0012aa6001f018806061fc8012a30cd07160fe890322f6750122f6456012de124669ff806080c53f0112ca610280153f0112e080153f0112ee80153f0112e86020f018a30e7eff80e080046100d0113e00123012de78ff48fe68ff12ee7801480268016004f01869ff1270a314f533f265f12963376400d3457305f229d34500eef0008000fc00aa0000000000',
+                    }, {
+                        name: 'Space Invaders',
+                        rom: '1225535041434520494e56414445525320302e39312042792044617669642057494e544552600061006208a3ddd0187108f21e3120122d700861003040122d69056c156e002391600af015f0073000124b23917e0112456600681c69006a046b0a6c046d3c6e0f00e023752351fd156004e09e127d2375380078ff23756006e09e128b23753839780123753600129f6005e09e12e96601651b8480a3d9d451a3d9d45175ff35ff12ad660012e9d4513f0112e9d45166008340730383b562f883226208330012c9237d8206430812d3331012d5237d8206331812dd237d8206432012e7332812e9237d3e0013077906491869006a046b0a6c047df46e0f00e023512375fd15126ff7073700126ffd1523518ba43b12131b7c026afc3b0213237c026a0423513c18126f00e0a4dd60146108620fd01f7008f21e302c133360fff015f00730001341f00a00e0a706fe651225a3c1f91e610823698106236981062369810623697bd000ee80e080123000dbc67b0c00eea3d9601cd80400ee23518e2323516005f018f015f0073000138900ee6a008de06b04e9a11257a60cfd1ef06530ff13af6a006b046d016e011397a50af01edbc67b087d017a013a07139700ee3c7effff99997effff2424e77eff3c3c7edb81423c7effdb10387cfe00007f003f007f0000000101010303030300003f20202020202020203f0808ff0000fe00fc00fe0000007e4242626262620000ff0000000000000000ff0000ff007d00417d057d7d0000c2c2c6446c28380000ff0000000000000000ff0000ff00f71014f7f7040400007c44fec2c2c2c20000ff0000000000000000ff0000ff00ef2028e8e82f2f0000f985c5c5c5c5f90000ff0000000000000000ff0000ff00be00203020bebe0000f704e7858584f40000ff0000000000000000ff0000ff00007f003f007f000000ef28ef00e0606f0000ff0000000000000000ff0000ff0000fe00fc00fe000000c000c0c0c0c0c00000fc0404040404040404fc1010fff981b98b9a9afa00fa8a9a9a9b99f8e62525f434343400171434373626c7df50505cd8d8df00df111f121b19d97c44fe868686fc84fe8282fefe80c0c0c0fefc82c2c2c2fcfe80f8c0c0fefe80f0c0c0c0fe80be8686fe8686fe8686861010101010101818184848789c90b0c0b09c8080c0c0c0feee9292868686fe82868686867c828686867cfe82fec0c0c07c82c2cac47afe86fe909c84fec0fe0202fefe10303030308282c2c2c2fe828282ee38108686969292ee8244383844828282fe303030fe021ef080fe0000000006060000006060c00000000000001818181800187cc60c1800180000fefe0000fe82868686fe080808181818fe02fec0c0fefe021e0606fe84c4c4fe0404fe80fe0606fec0c0c0fe82fefe02020606067c44fe8686fefe82fe06060644fe4444fe44a8a8a8a8a8a8a86c5a000c18a8304e7e001218666ca85a665424660048481812a80690a812007e3012a884304e721866a8a8a8a8a8a8905478a848786c72a812186c72665490a8722a18a8304e7e001218666ca87254a85a66187e184e72a8722a183066a8304e7e006c30544e9ca8a8a8a8a8a8a848547e18a890547866a86c2a305aa88430722aa8d8a8004e12a8e4a2a8004e12a86c2a545472a88430722aa8de9ca8722a18a80c54485a78721866a866185a5466726ca8722a0072a8722a18a8304e7e001218666ca8006618a8304e0c6618006c304e24a8722a183066a81e54660c189ca824545412a842780c3ca8aea8a8a8a8a8a8a8ff000000000000000000000000000000',
+                    }, {
+                        name: 'IBM Logo',
+                        rom: '00e0a22a600c6108d01f7009a239d01fa2487008d01f7004a257d01f7008a266d01f7008a275d01f1228ff00ff003c003c003c003c00ff00ffff00ff0038003f003f003800ff00ff8000e000e00080008000e000e00080f800fc003e003f003b003900f800f8030007000f00bf00fb00f300e30043e000e0008000800080008000e000e0',
+                    }, {
+                        name: 'Random Number Test',
+                        rom: '00e0c0ffa224f033f265f02960006300d035f1296005d035f229600ad035f00a1200',
+                    },
+                ],
+                rom: '',
+
+                showDebug: false,
+                showRom: false,
+                showKeysDown: false,
+                showCurrentTick: true,
+                showCpu: false,
+                showMemory: false,
+                showGrid: true,
+                gridX: 0,
+                gridY: 0,
+
+                // debugging handles
+                emulator, cpu, memory, audio, video, input,
+            }
+        },
+        mounted() {
+            emulator.init(this.$refs.canvas);
+        },
+        methods: {
+            async importRom(event) {
+                let file = new Uint8Array(await event.target.files?.[0]?.arrayBuffer());
+
+                this.rom = binToHex(file);
+                this.loadRom();
+            },
+            loadRom() {
+                let rom = hexToBin(this.rom);
+                emulator.loadRom(rom);
+
+                emulator.start();
+            },
+            pause() {
+                emulator.pause();
+            },
+            start() {
+                emulator.start();
+            },
+            jump() {
+                cpu.pc+=2;
+            },
+        }
+    }
 </script>
+<style scoped lang="scss">
+    .canvas-wrapper {
+        position: relative;
+        width: 512px;
+        height: 256px;
+    }
+
+    .grid {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        display: grid;
+        grid-template-columns: repeat(64, 1fr);
+        grid-template-rows: repeat(32, 1fr);
+        grid-gap: 0;
+
+        div {
+            border-left: 1px solid #222;
+            border-top: 1px solid #222;
+        }
+    }
+
+    canvas {
+        background-color: #000;
+        width: 512px;
+        height: 256px;
+        image-rendering: pixelated;
+    }
+</style>
